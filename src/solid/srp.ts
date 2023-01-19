12454c00ee8c113fd2efbf7160c2f29344ecea15
Isaac Alfaro <isaac16cr@outlook.com>
2023-01-19 17:26:28 -0600

    srp:after

--- src/solid/srp.ts
+++ src/solid/srp.ts
@@ -1,31 +1,68 @@
 (() => {
+  // Interfaces
   interface Product {
     id: number;
     name: string;
   }
 
-  class ProductBloc {
-    loadProduct(id: number) {
+  // Services
+  class ProductService {
+    getProduct(id: number) {
       console.log('Product: ', { id, name: 'OLED Tv' });
     }
 
     saveProduct(product: Product) {
       console.log('Saving...', product);
     }
+  }
 
-    notifyClients() {
+  class NotificationService {
+    sendEmail(emailList: string[], title: String, content: string) {
       console.log('Sending emails...');
     }
+  }
 
+  // Classes
+  class ProductBloc {
+    private productService: ProductService;
+    private notificationService: NotificationService;
+
+    constructor(
+      productService: ProductService,
+      notificationService: NotificationService
+    ) {
+      this.productService = productService;
+      this.notificationService = notificationService;
+    }
+
+    loadProduct(id: number) {
+      this.productService.getProduct(id);
+    }
+
+    saveProduct(product: Product) {
+      this.productService.saveProduct(product);
+    }
+
+    notifyClients() {
+      this.notificationService.sendEmail([], '', '');
+    }
+  }
+
+  class CartBloc {
     onAddToCart(productId: number) {
       console.log('Adding to cart ', productId);
     }
   }
 
-  const productBloc = new ProductBloc();
+  // Implement
+  const productService = new ProductService();
+  const notificationService = new NotificationService();
+
+  const productBloc = new ProductBloc(productService, notificationService);
+  const cartBloc = new CartBloc();
 
   productBloc.loadProduct(10);
   productBloc.saveProduct({ id: 10, name: 'OLED TV' });
   productBloc.notifyClients();
-  productBloc.onAddToCart(10);
+  cartBloc.onAddToCart(10);
 })();
